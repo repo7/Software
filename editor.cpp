9ef10e012f72575982acbfe34644f34b4fab4371
repo7 <liberty7479@naver.com>
2018-02-05 13:43:45 +0900

    Create editor.cpp
    L make a left on cursor 
    D make a right on cursor
    B delete a char left on cursor *cursor always on the right side 
    p add a char *cursor always on the right side

---
+++ editor.cpp
@@ -0,0 +1,71 @@
+#include <Windows.h>
+#include <iostream>
+#include <list>
+#include <string>
+
+using namespace std;
+void print(list<char> editor)
+{
+	for (char c : editor) {
+
+		cout << c;
+
+	}
+	printf("\n");
+}
+int main()
+{
+	string s;
+	cin >> s;
+
+	list<char> editor(s.begin(), s.end());
+	auto cursor = editor.end();
+
+	int n; cin >> n;
+	if (0 < n || n < 10) {
+
+		while (n--) {
+			char cmd;
+			cin >> cmd;
+			if (cmd == 'L' || cmd == 'l') {
+
+				if (cursor != editor.begin()) {
+
+					--cursor;
+				}
+			}
+			else if (cmd == 'D' || cmd == 'd') {
+
+				if (cursor != editor.end()) {
+
+					++cursor;
+				}
+			}
+			else if (cmd == 'P' || cmd == 'p') {
+
+				char c;
+				cin >> c;
+				editor.insert(cursor, c);
+			}
+			else if (cmd == 'B' || cmd == 'b') {
+
+				if (cursor != editor.begin()) {
+
+					auto temp = cursor;
+					--cursor;
+					editor.erase(cursor);
+					cursor = temp;
+				}
+			}
+		
+			print(editor);
+		
+		}
+		
+	}
+	else {
+		printf("error");
+	}
+	system("pause");
+	return 0;
+}
