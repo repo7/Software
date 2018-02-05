083e49a3401b5eb09f2070aa47693b7fefde7c7d
repo7 <liberty7479@naver.com>
2018-02-05 13:30:38 +0900

    Create list_erase.cpp
    The initial list is: 10 20 30 40 50
    After erasing the first element, the list becomes: 20 30 40 50
    After erasing all elements but the first, the list becomes:  20

---
+++ list_erase.cpp
@@ -0,0 +1,34 @@
+// list_erase.cpp
+// compile with: /EHsc
+#include <list>
+#include <iostream>
+
+int main( ) 
+{
+   using namespace std;
+   list <int> c1;
+   list <int>::iterator Iter;
+
+   c1.push_back( 10 );
+   c1.push_back( 20 );
+   c1.push_back( 30 );
+   c1.push_back( 40 );
+   c1.push_back( 50 );
+   cout << "The initial list is:";
+   for ( Iter = c1.begin( ); Iter != c1.end( ); Iter++ )
+      cout << " " << *Iter;
+   cout << endl;
+
+   c1.erase( c1.begin( ) );
+   cout << "After erasing the first element, the list becomes:";
+   for ( Iter = c1.begin( ); Iter != c1.end( ); Iter++ )
+      cout << " " << *Iter;
+   cout << endl;
+   Iter = c1.begin( );
+   Iter++;
+   c1.erase( Iter, c1.end( ) );
+   cout << "After erasing all elements but the first, the list becomes: ";
+   for (Iter = c1.begin( ); Iter != c1.end( ); Iter++ )
+      cout << " " << *Iter;
+   cout << endl;
+}
