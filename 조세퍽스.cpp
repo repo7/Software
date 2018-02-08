99f3e89a017a8c9051c31387f2fefb9fb09739fb
repo7 <liberty7479@naver.com>
2018-02-08 13:27:49 +0900

    Update 조세퍽스.cpp
    7 3
    <4 1 6 5 7 3 2>

--- 조세퍽스.cpp
+++ 조세퍽스.cpp
@@ -1,56 +1,35 @@
 #include <iostream>
-
 #include <queue>
+#include <Windows.h>
 
 using namespace std;
 
-int main() {
-
+int main()
+{
 	int n, m;
-
 	cin >> n >> m;
-
-
-
 	queue<int> q;
-
-	for (int i = 1; i <= n; i++) {
-
-		q.push(i);
-
+	for (int i = 0; i < n; i++)
+	{
+		q.push(i+1);
 	}
-
-
-
-	cout << '<';
-
-	for (int k = 1; k <= n; k++) {
-
-		for (int i = 0; i<m - 1; i++) {
-
+	printf("<");
+	while (!q.empty())
+	{
+		for (int i = 1; i <= m; i++)
+		{
 			q.push(q.front());
-
 			q.pop();
-
 		}
-		// -[]-[]-[]- all nodes connected circly 
-
 		cout << q.front();
-
-		q.pop();
-
-		if (k != n) {
-
-			cout << ", ";
-
+		if (q.size() == 1) {
+			printf(">");
 		}
-
+		else {
+			printf(" ");
+		}
+		q.pop();
 	}
-
-	cout << '>' << '\n';
-
-
-
+	system("pause");
 	return 0;
-
 }
