dfc423561006543b7fe799144e2162b593ba2943
repo7 <liberty7479@naver.com>
2018-02-08 13:51:16 +0900

    문장내단어개수.cpp
    while문 내 성공적으로 입력받으면 true를 리턴하는데 
    while문 last updated cnt값을 스코프 밖으로 어떻게 가지고 와야할지 모르겠다(...)

---
+++ 문장내단어개수.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	int cnt =0 ;
+	while (cin >> s)
+	{
+		cnt += 1;
+		cout << cnt << '\n';
+	}
+
+	system("pause");
+	return 0;
+}
